24dfd6765d31e7df89beb0c050648f65d0b82a69
Anish Mishra <anishkumarc92@gmail.com>
2024-08-03 01:54:46 +0530

    api fetch start

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,15 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       limit: '50',
+//       offset: '0',
+//       orderBy: '24hVolume',
+//       orderDirection: 'desc'
+//     },
+//     headers: {
+//       'x-rapidapi-key': '0fa6e6b421msh110851df8340dcfp174662jsn01bc771dc4a4',
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
