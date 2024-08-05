b326ae3e261166e02be4a0217bee3cea0996897b
Anish Mishra <anishkumarc92@gmail.com>
2024-08-06 01:49:08 +0530

    crypto news (api issue!)

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,20 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
     'x-rapidapi-key': '0fa6e6b421msh110851df8340dcfp174662jsn01bc771dc4a4',
     'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
     'X-BingApis-SDK': 'true'
-  }
-  const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+}
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
 
-  const createRequest = (url) => ({url, headers: cryptoNewsHeaders})
+const createRequest = (url) => ({url, headers: cryptoNewsHeaders})
 
-  export const cryptoNewsApi = createApi({
+export const cryptoNewsApi = createApi({
     reducerPath: 'cryptoNewsApi',
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints: (builder)=>({
         getCryptoNews: builder.query({
-            query: ({newsCategory, count})=>createRequest(`/coins?limit=${count}`),
-        })
-    })
+            query: ({newsCategory, count})=>createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        }),
+    }),
 });
+
+export const {
+    useGetCryptoNewsQuery,
+} = cryptoNewsApi; 
